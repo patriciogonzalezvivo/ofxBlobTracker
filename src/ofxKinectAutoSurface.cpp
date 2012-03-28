2fd386cd87d39da3ed15a9ea7f8d49c713a40b95
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2012-03-28 20:31:46 -0300

    repair some changes

--- src/ofxKinectAutoSurface.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ofxKinectAutoSurface.cpp
-//  mdt-Core
-//
-//  Created by Patricio González Vivo on 3/28/12.
-//  Copyright (c) 2012 PatricioGonzalezVivo.com. All rights reserved.
-//
-
-#include <iostream>
